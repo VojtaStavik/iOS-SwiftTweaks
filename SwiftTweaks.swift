113e8efa27196dec2e1ba6a1bec6f46d569731ed
Vojta Stavik <vojta@ulikeit.com>
2015-02-20 19:15:47 +0100

    Random string

--- SwiftTweaks.swift
+++ SwiftTweaks.swift
@@ -44,6 +44,22 @@ func between<T : Comparable>(minimum: T, maximum: T, value: T) -> T {
 }
 
 
+func randomStringWithLength (len : Int) -> NSString {
+    
+    let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    
+    var randomString : NSMutableString = NSMutableString(capacity: len)
+    
+    for (var i=0; i < len; i++){
+        var length = UInt32 (letters.length)
+        var rand = arc4random_uniform(length)
+        randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
+    }
+    
+    return randomString
+}
+
+
 extension String {
     
     var uppercaseString : String {
