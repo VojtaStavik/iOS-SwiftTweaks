8f9a32ee28766208e2ac95f1c78a0abbe8dc44f4
Vojta Stavik <vojta@ulikeit.com>
2015-04-16 15:11:40 +0200

    syntax fixes

--- LogService.swift
+++ LogService.swift
@@ -5,7 +5,7 @@
 //  Copyright (c) 2015 STRV. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 let log = LogService()
 
@@ -60,21 +60,5 @@ class LogService {
         var messageText = "\n ERROR! \n \(filename).\(function)[\(line)]: \n Message: \(text) \n Error description: \(errorText)"
         
         self.message(messageText)
-    }
-    
-    
-    func errorWithAlert(text: String, error: NSError?, _ file: String = __FILE__, _ function: String = __FUNCTION__, _ line: Int = __LINE__) {
-        
-        showAlert("ERROR", text: error?.localizedDescription == nil ? text : error!.localizedDescription)
-        self.error(text, error: error, file, function, line)
-    }
-
-    
-    func showAlert(title: String, text: String) {
-        
-        let alert = UIAlertController(title: title, message: text, preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-        
-        UIApplication.sharedApplication().keyWindow?.visibleViewController()?.presentViewController(alert, animated: true, completion: nil)
     }
 }
