1dbd72737d3c6e31920a028341151a2cd3f85873
Vojta Stavik <vojta@ulikeit.com>
2015-06-08 16:15:47 +0200

    Add UIColor init from hex string

--- SwiftTweaks.swift
+++ SwiftTweaks.swift
@@ -109,6 +109,15 @@ extension String {
 }
 
 
+extension UINavigationController {
+    
+    var rootViewController: UIViewController {
+        
+        return self.viewControllers.first as! UIViewController
+    }
+}
+
+
 extension Array {
 
     mutating func removeObject<U: Equatable>(object: U) {
@@ -214,54 +223,53 @@ extension UIImage {
 
 extension UIColor {
     
-    class func colorWithHexString (hex:String) -> UIColor {
-
+    convenience init(hexString: String) {
+        
         var red:   CGFloat = 0.0
         var green: CGFloat = 0.0
         var blue:  CGFloat = 0.0
         var alpha: CGFloat = 1.0
         
-        if hex.hasPrefix("#") {
+        if hexString.hasPrefix("#") {
             
-            let index   = advance(hex.startIndex, 1)
-            let hex     = hex.substringFromIndex(index)
+            let index   = advance(hexString.startIndex, 1)
+            let hex     = hexString.substringFromIndex(index)
             let scanner = NSScanner(string: hex)
             var hexValue: CUnsignedLongLong = 0
             
             if scanner.scanHexLongLong(&hexValue) {
                 
                 switch (count(hex)) {
-        
-                    case 3:
-                        red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
-                        green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
-                        blue  = CGFloat(hexValue & 0x00F)              / 15.0
-
-                    case 4:
-                        red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
-                        green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
-                        blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
-                        alpha = CGFloat(hexValue & 0x000F)             / 15.0
                     
-                    case 6:
-                        red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
-                        green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
-                        blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+                case 3:
+                    red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+                    green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+                    blue  = CGFloat(hexValue & 0x00F)              / 15.0
                     
-                    case 8:
-                        red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
-                        green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
-                        blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
-                        alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                case 4:
+                    red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                    green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                    blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                    alpha = CGFloat(hexValue & 0x000F)             / 15.0
                     
-                    default:
-                        print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
+                case 6:
+                    red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+                    blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+                    
+                case 8:
+                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                    
+                default:
+                    print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
+                    
                 }
                 
-            }
-            
-            else {
-            
+            } else {
+                
                 println("Scan hex error")
             }
             
@@ -270,7 +278,13 @@ extension UIColor {
             print("Invalid RGB string, missing '#' as prefix")
         }
         
-        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    
+    class func colorWithHexString (hex:String) -> UIColor {
+
+        return UIColor(hexString: hex)
     }
 }
 
