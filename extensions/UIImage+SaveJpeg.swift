249991c02d3bec6cec56b796397e4ba4b88b098e
Pavel Pokorný <pajkycz@gmail.com>
2015-12-10 16:04:52 +0100

    UIImage resize and save as jpeg to temp directory extensions

---
+++ extensions/UIImage+SaveJpeg.swift
@@ -0,0 +1,15 @@
+extension UIImage {
+
+    func saveJpegToTemp(quality: CGFloat = 0.8) -> String? {
+
+        let fileManager = NSFileManager.defaultManager()
+        let dir = NSTemporaryDirectory()
+        let fileName = NSProcessInfo.processInfo().globallyUniqueString
+        let filePathToWrite = "\(dir)/\(fileName).jpg"
+
+        let imageData = UIImageJPEGRepresentation(self, quality)
+        imageData?.writeToFile(filePathToWrite, atomically: false)
+
+        return fileManager.fileExistsAtPath(filePathToWrite) ? filePathToWrite : nil
+    }
+}
