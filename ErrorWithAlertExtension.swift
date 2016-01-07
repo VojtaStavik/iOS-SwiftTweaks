e39ac13d754991b02214f3e2c039e5d94ce70ebf
Vojta Stavik <vojta@strv.com>
2016-01-07 14:05:12 +0100

    Fix showing alert view when current VC is dismissing

--- ErrorWithAlertExtension.swift
+++ ErrorWithAlertExtension.swift
@@ -11,27 +11,38 @@ import UIKit
 // If you want to use swift tweaks with extension, set the EXTENSION flag to TRUE
 
 #if !EXTENSION
-
-@available(iOS 8.0, *)
-public extension LogService {
     
-    public func errorWithAlert(text: String, error: NSError?, _ file: String = __FILE__, _ function: String = __FUNCTION__, _ line: Int = __LINE__) {
+    @available(iOS 8.0, *)
+    public extension LogService {
         
-        showAlert("ERROR", text: error?.localizedDescription == nil ? text : error!.localizedDescription)
-        self.error(text, error: error, file, function, line)
+        public func errorWithAlert(text: String, error: NSError?, _ file: String = __FILE__, _ function: String = __FUNCTION__, _ line: Int = __LINE__) {
+            showAlert("ERROR", text: error?.localizedDescription ?? text)
+            self.error(text, error: error, file, function, line)
+        }
+        
+        public func showAlert(title: String, text: String, alert: UIAlertController? = nil) -> UIAlertController {
+            
+            let alertToShow : UIAlertController
+            
+            if let alert = alert {
+                alertToShow = alert
+            } else {
+                let alert = UIAlertController(title: title, message: text, preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                alertToShow = alert
+            }
+            
+            delay(0.1) { [weak self] in
+                if let topVC = UIApplication.sharedApplication().keyWindow?.visibleViewController() where topVC.isBeingDismissed() == false {
+                    topVC.presentViewController(alertToShow, animated: true, completion: nil)
+                } else {
+                    self?.showAlert(title, text: text, alert: alertToShow)
+                }
+            }
+            
+            return alertToShow
+        }
     }
     
-    
-    public func showAlert(title: String, text: String) -> UIAlertController {
-        
-        let alert = UIAlertController(title: title, message: text, preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-        
-        UIApplication.sharedApplication().keyWindow?.visibleViewController()?.presentViewController(alert, animated: true, completion: nil)
-        
-        return alert
-    }
-}
-
     
 #endif
--- swift-tweaks.podspec
+++ swift-tweaks.podspec
@@ -1,12 +1,12 @@
 Pod::Spec.new do |s|
   s.name = 'SwiftTweaks'
-  s.version = '3.0'
+  s.version = '3.0.1'
   s.license = 'MIT'
   s.summary = 'Set of helper classes and functions for every Xcode project.'
   s.homepage = 'https://github.com/VojtaStavik/iOS-SwiftTweaks'
   s.social_media_url = 'http://twitter.com/VojtaStavik'
   s.authors = { "Vojta Stavik" => "stavik@outlook.com" }
-  s.source = { :git => 'https://github.com/VojtaStavik/iOS-SwiftTweaks', :tag => s.version, :branch => 'Swift2' }
+  s.source = { :git => 'https://github.com/VojtaStavik/iOS-SwiftTweaks', :tag => s.version }
   s.ios.deployment_target = '7.0'
   s.source_files   = '*.swift'
   s.frameworks = 'UIKit'
